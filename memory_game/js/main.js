72ca1e848be5d7f01000c5a7c6225fe4bd7c3197
Son Truong <sntruong12@gmail.com>
2019-01-18 14:21:03 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running");
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+
+var cardOne = cards[2];
+var cardTwo = cards[3];
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You've found a match!");
+  } else {
+    alert("Sorry you're trash!")
+  }
+}
